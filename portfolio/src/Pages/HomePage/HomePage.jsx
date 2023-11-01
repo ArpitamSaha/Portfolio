33992a26f260f8dd5922752e4d31eaf84aa9425e
Arpitam Saha <arpitam972@gmail.com>
2023-11-01 21:02:37 +0530

    minor modification

---
+++ portfolio/src/Pages/HomePage/HomePage.jsx
@@ -0,0 +1,75 @@
+import React from "react";
+import "./HomePage.scss";
+
+import bg_vdo from "../../Images/bg-vdo.mp4";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// import { faGithub  } from '@fortawesome/free-solid-svg-icons'
+import {
+  faGithub,
+  faLinkedinIn,
+  faInstagram,
+} from "@fortawesome/free-brands-svg-icons";
+
+const HomePage = () => {
+  return (
+    <div>
+      <section className="home show-animate" id="home">
+        <video autoPlay loop muted className="bg-vdo" src={bg_vdo} />
+        <div className="home-content">
+          <h1>
+            Hi,I'm <br /> Arpitam Saha
+          </h1>
+          <div className="animated-text">
+            <span>Frontend Developer </span>
+          </div>
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quasi
+            voluptate impedit inventore harum eos modi beatae minus ut
+            consequatur. Architecto.
+          </p>
+          <div className="btn-box">
+            <a href="/about">About Me</a>
+          </div>
+        </div>
+        <div className="social-media">
+          <a
+            href="https://github.com/ArpitamSaha"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <FontAwesomeIcon
+              icon={faGithub}
+              size="2xl"
+              style={{ color: "#4a0962" }}
+            />
+          </a>
+          <a
+            href="http://www.linkedin.com/in/arpitamsaha"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <FontAwesomeIcon
+              icon={faLinkedinIn}
+              size="2xl"
+              style={{ color: "#4a0962" }}
+            />
+          </a>
+          <a
+            href="https://www.instagram.com/im.arpitam_saha"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <FontAwesomeIcon
+              icon={faInstagram}
+              size="2xl"
+              style={{ color: "#4a0962" }}
+            />
+          </a>
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default HomePage;
--- portfolio/src/Pages/PortfolioPage/PortfolioPage.js
+++ portfolio/src/Pages/PortfolioPage/PortfolioPage.js
@@ -1,88 +1,117 @@
-import React from 'react';
+import React from "react";
 
-import TempConvertor from '../../Images/Temp-convertor.jpg';
-import Hunation from '../../Images/Hunation.jpg';
-import GymWebsite from '../../Images/GYM Website.jpg';
-import NetflixClone from '../../Images/Netflix-Clone.jpg';
-import AmazonClone from '../../Images/Amazon-Clone.jpg';
-import DiceGame from '../../Images/DiceGame.jpg';
-import Plantlet from '../../Images/Plantlet.jpg';
-import ToDOList from '../../Images/To-Do-List.jpg';
+import TempConvertor from "../../Images/Temp-convertor.jpg";
+import Hunation from "../../Images/Hunation.jpg";
+import GymWebsite from "../../Images/GYM Website.jpg";
+import NetflixClone from "../../Images/Netflix-Clone.jpg";
+import AmazonClone from "../../Images/Amazon-Clone.jpg";
+import DiceGame from "../../Images/DiceGame.jpg";
+import Plantlet from "../../Images/Plantlet.jpg";
+import ToDOList from "../../Images/To-Do-List.jpg";
 
 import "./PortfolioPage.scss";
 const Portfolio = () => {
-    return (
-        <div>
-            <section class="portfolio" id="portfolio">
-                <h2 class="heading"><span>My</span> Portfolio</h2>
+  return (
+    <div>
+      <section class="portfolio" id="portfolio">
+        <h2 class="heading">
+          <span>My</span> Portfolio
+        </h2>
 
-                <div class="work-section">
-
-                    <div class="work left">
-                        <h2>Hunation</h2>
-                        <div class="work-img">
-                            <a href="/"><img src={Hunation} alt="Hunation" /></a>
-                        </div>
-                        <p>This is a restaurant Website made using React Js | CSS | Java Script</p>
-                    </div>
-                    <div class="work left">
-                        <h2>To Do List</h2>
-                        <div class="work-img">
-                            <a href="/"><img src={ToDOList} alt="Alternate" /></a>
-                        </div>
-                        <p>Daily To Do List made with html | CSS | Java Script</p>
-                    </div>
-                    <div class="work right">
-                        <h2>Temperature Convertor</h2>
-                        <div class="work-img">
-                            <a href="/"> <img src={TempConvertor} alt="Alternate" /></a>
-                        </div>
-                        <p>Celcius to Faranheit to Kelvin Convertor using Java Script</p>
-                    </div>
-                    <div class="work right">
-                        <h2>Plantlet</h2>
-                        <div class="work-img">
-                            <a href="/"><img src={Plantlet} alt="Alternate" /></a>
-                        </div>
-                        <p>Hackathon project built using React Js | Firebase</p>
-                    </div>
-                    <div class="work left">
-                        <h2>Dice Game</h2>
-                        <div class="work-img">
-                            <a href="/"><img src={DiceGame} alt="Alternate" /></a>
-                        </div>
-                        <p>A simple dice game made with HTML | CSS | Java Script</p>
-                    </div>
-                    <div class="work left">
-                        <h2>Get-Set-Fit</h2>
-                        <div class="work-img">
-                            <a href="/"><img src={GymWebsite} alt="Alternate" /></a>
-                        </div>
-                        <p>This is a restaurant Website made using React Js | CSS | Java Script</p>
-                    </div>
-                    <div class="work right">
-                        <h2>Netflix-Clone</h2>
-                        <div class="work-img">
-                            <a href="/"><img src={NetflixClone} alt="Alternate" /></a>
-                        </div>
-                        <p>This is a restaurant Website made using React Js | CSS | Java Script</p>
-                    </div>
-                    <div class="work right">
-                        <h2>Amazon-Clone</h2>
-                        <div class="work-img">
-                            <a href="/"><img src={AmazonClone} alt="Alternate" /></a>
-                        </div>
-                        <p>This is a restaurant Website made using React Js | CSS | Java Script</p>
-                    </div>
-
-                </div>
-
-                <div class="btn-box" style={{ textAlign: "center", marginTop: "2rem" }}>
-                    <a href="/contact">Contact</a>
-                </div>
-            </section>
+        <div class="work-section">
+          <div class="work ">
+            <h2>Hunation</h2>
+            <div class="work-img">
+              <a href="/">
+                <img src={Hunation} alt="Hunation" />
+              </a>
+            </div>
+            <p>
+              This is a restaurant Website made using React Js | CSS | Java
+              Script
+            </p>
+          </div>
+          <div class="work ">
+            <h2>To Do List</h2>
+            <div class="work-img">
+              <a href="/">
+                <img src={ToDOList} alt="Alternate" />
+              </a>
+            </div>
+            <p>Daily To Do List made with html | CSS | Java Script</p>
+          </div>
+          <div class="work">
+            <h2>Temperature Convertor</h2>
+            <div class="work-img">
+              <a href="/">
+                {" "}
+                <img src={TempConvertor} alt="Alternate" />
+              </a>
+            </div>
+            <p>Celcius to Faranheit to Kelvin Convertor using Java Script</p>
+          </div>
+          <div class="work">
+            <h2>Plantlet</h2>
+            <div class="work-img">
+              <a href="/">
+                <img src={Plantlet} alt="Alternate" />
+              </a>
+            </div>
+            <p>Hackathon project built using React Js | Firebase</p>
+          </div>
+          <div class="work ">
+            <h2>Dice Game</h2>
+            <div class="work-img">
+              <a href="/">
+                <img src={DiceGame} alt="Alternate" />
+              </a>
+            </div>
+            <p>A simple dice game made with HTML | CSS | Java Script</p>
+          </div>
+          <div class="work ">
+            <h2>Get-Set-Fit</h2>
+            <div class="work-img">
+              <a href="/">
+                <img src={GymWebsite} alt="Alternate" />
+              </a>
+            </div>
+            <p>
+              This is a restaurant Website made using React Js | CSS | Java
+              Script
+            </p>
+          </div>
+          <div class="work">
+            <h2>Netflix-Clone</h2>
+            <div class="work-img">
+              <a href="/">
+                <img src={NetflixClone} alt="Alternate" />
+              </a>
+            </div>
+            <p>
+              This is a restaurant Website made using React Js | CSS | Java
+              Script
+            </p>
+          </div>
+          <div class="work">
+            <h2>Amazon-Clone</h2>
+            <div class="work-img">
+              <a href="/">
+                <img src={AmazonClone} alt="Alternate" />
+              </a>
+            </div>
+            <p>
+              This is a restaurant Website made using React Js | CSS | Java
+              Script
+            </p>
+          </div>
         </div>
-    );
+
+        <div class="btn-box" style={{ textAlign: "center", marginTop: "2rem" }}>
+          <a href="/contact">Contact</a>
+        </div>
+      </section>
+    </div>
+  );
 };
 
 export default Portfolio;
